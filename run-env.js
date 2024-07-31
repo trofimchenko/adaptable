1e33a160191cba906b55918a41a7a7568a6552c4
trofimchenko <trofimchenko@gmail.com>
2024-07-31 13:58:42 +0300

    added env variable

---
+++ run-env.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+const child_process = require("child_process");
+
+const config = dotenv.config();
+const PORT = process.env.PORT || 4200;
+
+const child = child_process.exec(`ng serve --port=${PORT}`);
+child.stderr.on("data", (err) => console.error(err.toString()));
+child.stdout.on("data", (data) => console.log(data.toString()));

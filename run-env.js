e56e1bb7ff8062874f517482b0da0aa57994ca4c
trofimchenko <trofimchenko@gmail.com>
2024-07-31 14:37:02 +0300

    set configuration to production

--- run-env.js
+++ run-env.js
@@ -4,6 +4,8 @@ const child_process = require("child_process");
 const config = dotenv.config();
 const PORT = process.env.PORT || 4200;
 
-const child = child_process.exec(`ng serve --port=${PORT}`);
+const child = child_process.exec(
+  `ng serve --port=${PORT} --configuration production`
+);
 child.stderr.on("data", (err) => console.error(err.toString()));
 child.stdout.on("data", (data) => console.log(data.toString()));

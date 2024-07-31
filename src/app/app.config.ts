8223565c09e8e247222f4e4aad0cb1092b3887b6
trofimchenko <trofimchenko@gmail.com>
2024-07-31 15:08:03 +0300

    new initial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,8 @@
-import {
-  ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes),
-    provideClientHydration(),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
